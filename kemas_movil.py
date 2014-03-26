9f2352af3c23c48772ef4800ff4c0cb943d14c5a
etenesaca <edgartenesaca@gmail.com>
2014-03-26 16:12:45 -0500

    Mejorado el proceso de obtener los datos del Colaborator

--- kemas_movil.py
+++ kemas_movil.py
@@ -91,7 +91,7 @@ class kemas_collaborator(osv.osv):
                 
         sql = """
             SELECT 
-                Cl.id, Cl.personal_id, CL.code,CL.name,Cl.nick_name,Cl.birth,Cl.marital_status,Cl.address,Cl.photo_Large as image_medium,
+                Cl.id, Cl.personal_id, CL.code,CL.name,Cl.nick_name,Cl.birth,Cl.marital_status,Cl.address,
                 Cl.mobile,Cl.telef1,Cl.telef2,Cl.email,Cl.im_account,
                 Cl.join_date,CL.points,LV.name as level, CL.team_id, Cl.genre
             FROM kemas_collaborator as CL
@@ -132,9 +132,6 @@ class kemas_collaborator(osv.osv):
             else:
                 collaborator['marital_status'] = 'Casad' + lgenre[collaborator['genre']]
                     
-            # Poner la foto en el formato correcto
-            collaborator['image_medium'] = build_image(collaborator['image_medium'])
-        
             # Calcular la edad
             collaborator['age'] = kemas_extras.calcular_edad(collaborator['birth'])
             collaborator['birth'] = kemas_extras.convert_date_format_short_str(collaborator['birth'])
